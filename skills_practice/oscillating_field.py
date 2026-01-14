079ef6c7234d439c27ae4a1f3936a1a36834dc0d
williamw27-lab <williamw27@lakesideschool.org>
2026-01-14 14:00:22 -0800

    Create new simulation code

---
+++ skills_practice/oscillating_field.py
@@ -0,0 +1,9 @@
+### Create a simulation of a mobile electron and stationary proton in an oscillating external electric field
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.linalg import norm
+from scipy.integrate import solve_ivp
+import matplotlib.animation as animation
+
+# 
